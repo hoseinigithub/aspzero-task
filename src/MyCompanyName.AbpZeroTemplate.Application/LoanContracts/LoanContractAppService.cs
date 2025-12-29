4bb1b9241a77cd4ac6a38b5573eebc160a4b6d77
Hoseini <hoseini.fullstackdeveloper@gmail.com>
2025-12-29 12:45:51 +0330

    create loanContract

---
+++ src/MyCompanyName.AbpZeroTemplate.Application/LoanContracts/LoanContractAppService.cs
@@ -0,0 +1,61 @@
+﻿using Abp.Application.Services;
+using Abp.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class LoanContractAppService : ApplicationService
+{
+    private readonly IRepository<LoanContract, int> _loanRepo;
+    private readonly IRepository<LoanContractPerson, int> _relationRepo;
+
+    public LoanContractAppService(
+        IRepository<LoanContract, int> loanRepo,
+        IRepository<LoanContractPerson, int> relationRepo)
+    {
+        _loanRepo = loanRepo;
+        _relationRepo = relationRepo;
+    }
+
+    public async Task<List<LoanContract>> GetAll()
+    {
+        return await _loanRepo
+            .GetAll()
+            .Include(x => x.LoanContractPersons)
+            .ThenInclude(x => x.Person)
+            .ToListAsync();
+    }
+
+    public async Task Create(string title, int borrowerId, List<int> guarantorIds)
+    {
+        //  ایجاد قرارداد
+        var contract = await _loanRepo.InsertAsync(new LoanContract
+        {
+            ContractTitle = title,
+            ContractDate = DateTime.Now
+        });
+
+        await CurrentUnitOfWork.SaveChangesAsync(); 
+
+        //  اضافه کردن وام‌گیرنده
+        await _relationRepo.InsertAsync(new LoanContractPerson
+        {
+            LoanContractId = contract.Id,
+            PersonId = borrowerId,
+            Role = " وام‌گیرنده"
+        });
+
+        //  اضافه کردن ضامن‌ها
+        foreach (var gid in guarantorIds)
+        {
+            await _relationRepo.InsertAsync(new LoanContractPerson
+            {
+                LoanContractId = contract.Id,
+                PersonId = gid,
+                Role = "ضامن"
+            });
+        }
+    }
+
+}
---
+++ src/MyCompanyName.AbpZeroTemplate.Web.Mvc/Controllers/LoanContractsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyCompanyName.AbpZeroTemplate.Web.Controllers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class LoanContractsController : AbpZeroTemplateControllerBase
+{
+    private readonly LoanContractAppService _service;
+    private readonly PersonAppService _personService;
+
+    public LoanContractsController(
+        LoanContractAppService service,
+        PersonAppService personService)
+    {
+        _service = service;
+        _personService = personService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var data = await _service.GetAll();
+        return View(data);
+    }
+
+    public async Task<IActionResult> Create()
+    {
+        ViewBag.Persons = await _personService.GetAll();
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(string title, int borrowerId, List<int> guarantorIds)
+    {
+        await _service.Create(title, borrowerId, guarantorIds);
+        return RedirectToAction("Index");
+    }
+}
